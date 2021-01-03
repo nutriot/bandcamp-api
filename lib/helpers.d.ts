048e32932b3550166301504d87b7ca385bcae633
idleberg <git@idleberg.com>
2021-01-03 14:18:47 +0100

    delete files

--- lib/helpers.d.ts
+++
@@ -1,4 +0,0 @@
-declare function normalizeDate(body: object): object;
-declare function normalizeErrors(body: object): object;
-declare function queryStringify(object: object): string;
-export { normalizeDate, normalizeErrors, queryStringify };
