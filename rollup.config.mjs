f663db8f2d9719b1d882a7c70d9af8e9f27970fe
Jan T. Sott <git@idleberg.com>
2022-11-07 00:28:43 +0100

    simplify config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,25 +1,21 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from "@rollup/plugin-typescript";
 
-const plugins = [
-  typescript()
-];
+const plugins = [typescript()];
 
 export default [
-  {
-    input: 'src/index.ts',
-    output: {
-      exports: 'default',
-      file: './lib/index.cjs',
-      format: 'cjs'
-    },
-    plugins: plugins
-  },
-  {
-    input: 'src/index.ts',
-    output: {
-      file: './lib/index.mjs',
-      format: 'esm'
-    },
-    plugins: plugins
-  }
+	{
+		input: "src/index.ts",
+		output: [
+			{
+				file: "./lib/index.mjs",
+				format: "esm",
+			},
+			{
+				exports: "default",
+				file: "./lib/index.cjs",
+				format: "cjs"
+			}
+		],
+		plugins: plugins,
+	},
 ];
