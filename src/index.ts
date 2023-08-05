2b8b7cc9664abc731331727b87aac500361927ea
Jan T. Sott <git@idleberg.com>
2023-08-05 20:51:51 +0200

    update JSDoc types

--- src/index.ts
+++ src/index.ts
@@ -97,7 +97,7 @@ export default class Bandcamp {
 	 * List of the bands you have access to (either through artist accounts, label accounts, or partnerships)
 	 *
 	 * @param {string} accessToken
-	 * @param {number} [version] - version of the API
+	 * @param {number} [version] - (optional) version of the API
 	 * @returns {Promise<Response>}
 	 *
 	 * @example
@@ -107,7 +107,7 @@ export default class Bandcamp {
 	 *
 	 * @see {@link https://bandcamp.com/developer/account}
 	 */
-	public async getMyBands(accessToken: string, version: string | number = 1): Promise<Response> {
+	public async getMyBands(accessToken: string, version: 1 | (number & {}) = 1): Promise<Response> {
 		const requestUrl = `${this.BANDCAMP_BASE_URL}/api/account/${version}/my_bands`;
 
 		const payload = {
@@ -126,10 +126,10 @@ export default class Bandcamp {
 	 * @param {BandcampApi.SalesReportRequestBody} body
 	 * @param {number} body.band_id - the unique id of the band or label you are calling as or on behalf of
 	 * @param {number} [body.member_band_id] - (optional) the unique id of a band you wish to filter your results on, if you're calling as or on behalf of a label
-	 * @param {string} body.start_time -  the earliest UTC sale time an item could have and still be included in the results
-	 * @param {string} [body.end_time] - (optional) the latest UTC sale time an item could have and still be included in the results (default is the time of the call)
-	 * @param {string} body.format - (optional) the format you wish to receive results in - either 'csv' or 'json' (default is 'json')
-	 * @param {number} [version] - version of the API
+	 * @param {Date | string} body.start_time -  the earliest UTC sale time an item could have and still be included in the results
+	 * @param {Date | string} [body.end_time] - (optional) the latest UTC sale time an item could have and still be included in the results (default is the time of the call)
+	 * @param {string} [body.format] - (optional) the format you wish to receive results in - either 'csv' or 'json' (default is 'json')
+	 * @param {number} [version] - (optional) version of the API
 	 * @returns {Promise<Response>}
 	 *
 	 * @example
@@ -144,7 +144,7 @@ export default class Bandcamp {
 	 * @see {@link https://bandcamp.com/developer/sales}
 		* ```
 	 */
-	public async getSalesReport(accessToken: string, body: BandcampApi.SalesReportRequestBody, version: string | number = 1): Promise<Response> {
+	public async getSalesReport(accessToken: string, body: BandcampApi.SalesReportRequestBody, version: 1 | 2 | (number & {}) = 2): Promise<Response> {
 		const requestUrl = `${this.BANDCAMP_BASE_URL}/api/sales/${version}/sales_report`;
 
 		const payload = {
@@ -167,7 +167,7 @@ export default class Bandcamp {
 	 * @param {string} body.start_time - earliest date the items you are interested in would have been added to Bandcamp
 	 * @param {string} [body.end_time] - (optional) latest date items you are in interested in would have been added to Bandcamp; defaults to the time of the call
 	 * @param {array} [body.package_ids] - (optional) an array of package IDs that you wish to filter your results on
-	 * @param {number} [version] - version of the API
+	 * @param {number} [version] - (optional) version of the API
 	 * @returns {Promise<Response>}
 	 *
 	 * @example
@@ -181,7 +181,7 @@ export default class Bandcamp {
 	 *
 	 * @see {@link https://bandcamp.com/developer/merch#get_merch_details}
 	 */
-	public async getMerchDetails(accessToken: string, body: BandcampApi.GetMerchDetailsRequestBody, version: string | number = 1): Promise<Response> {
+	public async getMerchDetails(accessToken: string, body: BandcampApi.GetMerchDetailsRequestBody, version: 1 | (number & {}) = 1): Promise<Response> {
 		const requestUrl = `${this.BANDCAMP_BASE_URL}/api/merchorders/${version}/get_merch_details`;
 
 		const payload = {
@@ -201,7 +201,7 @@ export default class Bandcamp {
 	 * @param {BandcampApi.GetShippingOriginRequestBody} [body]
 	 * @param {number} [body.band_id] - (optional) Bandcamp ID of your label or the (usually) label on whose behalf you are querying (get this ID from my_bands in the Account API)
 	 * @param {number} [body.origin_id] - (optional) Bandcamp ID of a specific shipping origin you want to retrieve details for
-	 * @param {number} [version] - version of the API
+	 * @param {number} [version] - (optional) version of the API
 	 * @returns {Promise<Response>}
 	 *
 	 * @example
@@ -211,7 +211,7 @@ export default class Bandcamp {
 	 *
 	 * @see {@link https://bandcamp.com/developer/sales}
 	 */
-	public async getShippingOriginDetails(accessToken: string, body: BandcampApi.GetShippingOriginRequestBody = {}, version: string | number = 1): Promise<Response> {
+	public async getShippingOriginDetails(accessToken: string, body: BandcampApi.GetShippingOriginRequestBody = {}, version: 1 | (number & {}) = 1): Promise<Response> {
 		const requestUrl = `${this.BANDCAMP_BASE_URL}/api/merchorders/${version}/get_shipping_origin_details`;
 
 		const payload = {
@@ -237,7 +237,7 @@ export default class Bandcamp {
 	 * @param {string} [body.name] - (optional) filter orders on this item name (or title)
 	 * @param {number} [body.origin_id] - (optional) filter orders on a particular shipping origin
 	 * @param {string} [body.format] - (optional) results format: 'csv' or 'json' (default is 'json')
-	 * @param {number} [version] - version of the API
+	 * @param {number} [version] - (optional) version of the API
 	 * @returns {Promise<Response>}
 	 *
 	 * @example
@@ -247,7 +247,7 @@ export default class Bandcamp {
 	 *
 	 * @see {@link https://bandcamp.com/developer/sales}
 	 */
-	public async getOrders(accessToken: string, body: BandcampApi.GetOrdersRequestBody, version = 3): Promise<Response> {
+	public async getOrders(accessToken: string, body: BandcampApi.GetOrdersRequestBody, version: 1 | 2 | 3 | (number & {}) = 3): Promise<Response> {
 		const requestUrl = `${this.BANDCAMP_BASE_URL}/api/merchorders/${version}/get_orders`;
 
 		const payload = {
@@ -266,13 +266,14 @@ export default class Bandcamp {
 	 * @param {string} accessToken
 	 * @param {BandcampApi.UpdateShippedItems[]} items - array of payments or sale items to update
 	 * @param {number} items[].id - unique Bandcamp ID of the payment or sale item to update
-	 * @param {number} items[].id_type - 'p' when id parameter refers to a payment, 's' for sale item
+	 * @param {'p' | 's'} items[].id_type - 'p' when id parameter refers to a payment, 's' for sale item
 	 * @param {boolean} [items[].shipped] - (optional), true to mark as shipped, false to mark as unshipped, missing or null (defaults to true)
 	 * @param {boolean} [items[].notification] - (optional) true to send notification, false don't, null or missing to honor seller (band or label) default setting
 	 * @param {string} [items[].notification_message] - (optional) custom message to send with shipping notificaton to buyer
-	 * @param {string} [items[].ship_date] - (optional) date of shipment; defaults to current date
+	 * @param {Date | string} [items[].ship_date] - (optional) date of shipment; defaults to current date
 	 * @param {string} [items[].carrier] - (optional) name of the shipping carrier (displayed to buyer)
-	 * @param {string} [items[].tracking_code] - (optional) tracking code or number (displayed to buyer)
+	 * @param {number | string} [items[].tracking_code] - (optional) tracking code or number (displayed to buyer)
+	 * @param {number} [version] - (optional) version of the API
 	 * @returns {Promise<Response>}
 	 *
 	 * @example
@@ -297,7 +298,7 @@ export default class Bandcamp {
 	 *
 	 * @see {@link https://bandcamp.com/developer/sales}
 	 */
-	public async updateShipped(accessToken: string, items: BandcampApi.UpdateShippedItems[], version = 2): Promise<Response> {
+	public async updateShipped(accessToken: string, items: BandcampApi.UpdateShippedItems[], version: 1 | 2 | (number & {}) = 2): Promise<Response> {
 		const requestUrl = `${this.BANDCAMP_BASE_URL}/api/merchorders/${version}/update_shipped`;
 
 		const payload = {
@@ -315,13 +316,13 @@ export default class Bandcamp {
 	 *
 	 * @param {string} accessToken
 	 * @param {BandcampApi.MarkDateRangeAsShippedRequestBody} body - array of payments or sale items to update
-	 * @param {number} body.id - identifies the label you're calling on behalf of
-	 * @param {number} body.member_band_id - (optional) identifies the band or artist to filter on
-	 * @param {string} [body.start_time] - (optional) earliest date in range of orders
-	 * @param {string} body.end_time - most recent date in range of orders (must be in past)
-	 * @param {number} body.origin_id - (optional) Bandcamp ID of a specific origin which the items you want to update were shipped from
-	 * @param {boolean} body.email_notifications - (optional) true to send notifications, false to suppress, leave out (or send null) to honor selling band preferences
-	 * @param {number} [version] - version of the API
+	 * @param {number} body.band_id - identifies the label you're calling on behalf of
+	 * @param {number} [body.member_band_id] - (optional) identifies the band or artist to filter on
+	 * @param {Date | string} [body.start_time] - (optional) earliest date in range of orders
+	 * @param {Date | string} body.end_time - most recent date in range of orders (must be in past)
+	 * @param {number} [body.origin_id] - (optional) Bandcamp ID of a specific origin which the items you want to update were shipped from
+	 * @param {boolean} [body.email_notifications] - (optional) true to send notifications, false to suppress, leave out (or send null) to honor selling band preferences
+	 * @param {number} [version] - (optional) version of the API
 	 * @returns {Promise<Response>}
 	 *
 	 * @example
@@ -337,7 +338,7 @@ export default class Bandcamp {
 	 *
 	 * @see {@link https://bandcamp.com/developer/sales}
 	 */
-	public async markDateRangeAsShipped(accessToken: string, body: BandcampApi.MarkDateRangeAsShippedRequestBody, version: string | number = 1): Promise<Response> {
+	public async markDateRangeAsShipped(accessToken: string, body: BandcampApi.MarkDateRangeAsShippedRequestBody, version: 1 | (number & {}) = 1): Promise<Response> {
 		const requestUrl = `${this.BANDCAMP_BASE_URL}/api/merchorders/${version}/mark_date_range_as_shipped`;
 
 		const payload = {
@@ -356,10 +357,10 @@ export default class Bandcamp {
 	 * @param {string} accessToken
 	 * @param {BandcampApi.UpdateQuantitiesRequestBody[]} items - array of items or item-options to update, where each array item is structured
 	 * @param {number} items[].id - package (merch item) or option (merch item-option) ID
-	 * @param {string} items[].id_type - "p" if id is for an item (or package), or "o" for an item-option
+	 * @param {'p' | 'o'} items[].id_type - "p" if id is for an item (or package), or "o" for an item-option
 	 * @param {number} items[].quantity_sold - the number of items that Bandcamp has sold, as reported by get_merch_details
 	 * @param {number} items[].quantity_available - the new inventory level you want to set
-	 * @param {number} items[].origin_id - (optional) Bandcamp ID of a specific origin which the items you want to update were shipped from
+	 * @param {number} [items[].origin_id] - (optional) Bandcamp ID of a specific origin which the items you want to update were shipped from
 	 * @param {number} [version] - version of the API
 	 * @returns {Promise<Response>}
 	 *
@@ -385,7 +386,7 @@ export default class Bandcamp {
 	 *
 	 * @see {@link https://bandcamp.com/developer/sales}
 	 */
-	public async updateQuantities(accessToken: string, items: BandcampApi.UpdateQuantitiesRequestBody[], version: string | number = 1): Promise<Response> {
+	public async updateQuantities(accessToken: string, items: BandcampApi.UpdateQuantitiesRequestBody[], version: 1 | (number & {}) = 1): Promise<Response> {
 		const requestUrl = `${this.BANDCAMP_BASE_URL}/api/merchorders/${version}/update_quantities`;
 
 		const payload = {
@@ -402,9 +403,9 @@ export default class Bandcamp {
 	 * Updates merch item stock-keeping units (SKU)
 	 *
 	 * @param {string} accessToken
-	 * @param {Object[]} items - array of items or item-options to update, where each array item is structured
+	 * @param {BandcampApi.UpdateSKURequestBody[]} items - array of items or item-options to update, where each array item is structured
 	 * @param {number} items[].id - merch item (package) and option ID
-	 * @param {string} items[].id_type - "p" if id refers to a merch item (package), or "o" an item-option
+	 * @param {'p', 'o'} items[].id_type - "p" if id refers to a merch item (package), or "o" an item-option
 	 * @param {string} items[].sku - the new SKU for the merch item (package) or item-option
 	 * @param {number} [version] - version of the API
 	 * @returns {Promise<Response>}
@@ -427,7 +428,7 @@ export default class Bandcamp {
 	 *
 	 * @see {@link https://bandcamp.com/developer/sales}
 	 */
-	public async updateSKU(accessToken: string, items: BandcampApi.UpdateSKURequestBody[], version: string | number = 1): Promise<Response> {
+	public async updateSKU(accessToken: string, items: BandcampApi.UpdateSKURequestBody[], version: 1 | (number & {}) = 1): Promise<Response> {
 		const requestUrl = `${this.BANDCAMP_BASE_URL}/api/merchorders/${version}/update_sku`;
 
 		const payload = {
