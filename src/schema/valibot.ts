9995dcc97bc51ce5156382cb257aab00deeffe7e
Jan T. Sott <git@idleberg.com>
2023-11-10 00:10:00 +0100

    update schemas

--- src/schema/valibot.ts
+++ src/schema/valibot.ts
@@ -1,18 +1,19 @@
-import { array, boolean, date, enumType, literal, number, object, optional, string, tuple } from 'valibot';
+import { array, boolean, date, email, enumType, integer, isoDate, literal, minLength, minValue, null_, number, object, optional, record, string, tuple, union, url } from 'valibot';
 
+const currency = number([minValue(0)]);
 const format = optional(enumType(['csv', 'json']));
 const dateString = tuple([date(), string()]);
 const idType = enumType(['o', 'p'])
 
 const sharedCredentialProps = {
 	grant_type: literal('client_credentials'),
-	client_id: number(),
+	client_id: number([integer()]),
 	client_secret: string()
 };
 
 const sharedSalesMerchProps = {
-	band_id: number(),
-	member_band_id: optional(number()),
+	band_id: number([integer()]),
+	member_band_id: optional(number([integer()])),
 	start_time: dateString,
 	end_time: optional(dateString)
 };
@@ -35,6 +36,26 @@ export const refreshToken = {
 };
 
 /**
+ * Account API
+ * @see {@link https://bandcamp.com/developer/account}
+ */
+export const getMyBands = {
+	requestBody: undefined,
+	reponse: object({
+		bands: array(object({
+			subdomain: string(),
+			band_id: number([integer()]),
+			name: string(),
+			member_bands: object({
+				subdomain: string(),
+				band_id: number([integer()]),
+				name: string()
+			})
+		}))
+	})
+};
+
+/**
  * Sales API
  * @see {@link https://bandcamp.com/developer/sales}
  */
@@ -43,7 +64,75 @@ export const salesReport = {
 		...sharedSalesMerchProps,
 		format: format
 	}),
-	response: undefined
+	response: record(string([minLength(1)]), object({
+		date: string([isoDate()]),
+		paid_to: string(),
+		item_type: tuple([literal('album'), literal('track')]),
+		item_name: string(),
+		artist: string(),
+		currency: union([
+			literal('AUD'),
+			literal('CAD'),
+			literal('CHF'),
+			literal('CZK'),
+			literal('DKK'),
+			literal('EUR'),
+			literal('GBP'),
+			literal('HKD'),
+			literal('HUF'),
+			literal('ILS'),
+			literal('JPY'),
+			literal('MXN'),
+			literal('NOK'),
+			literal('NZD'),
+			literal('PLN'),
+			literal('SEK'),
+			literal('SGD'),
+			literal('USD')
+		]),
+		item_price: currency,
+		quantity: number([integer(), minValue(0)]),
+		discount_code: tuple([null_(), string()]),
+		sub_total: currency,
+		// seller_tax: null,
+		// marketplace_tax: null,
+		// shipping: null,
+		// ship_from_country_name: null,
+		transaction_fee: currency,
+		// fee_type: paypal,
+		item_total: currency,
+		amount_you_received: currency,
+		bandcamp_transaction_id: number([integer()]),
+		// // paypal_transaction_id: null,
+		// net_amount: currency,
+		// // package: digital download,
+		// // option: null,
+		item_url: string([url()]),
+		// // catalog_number: null,
+		upc: string(),
+		isrc: string(),
+		buyer_name: string(),
+		buyer_email: string([email()]),
+		// // buyer_phone: null,
+		// // buyer_note: null,
+		ship_to_name: string(),
+		ship_to_street: string(),
+		ship_to_street_2: string(),
+		// // ship_to_city: null,
+		// // ship_to_state: null,
+		// // ship_to_zip: null,
+		// // ship_to_country: null,
+		// // ship_to_country_code: null,
+		// // ship_date: null,
+		// // ship_notes: null,
+		// // country: United States,
+		// // country_code: US,
+		// // region_or_state: IL,
+		city: string(),
+		referer: string(),
+		// referer_url: bandcamp.com/app/android/search,
+		// // sku: null
+	}))
 };
 
 /**
@@ -53,28 +142,28 @@ export const salesReport = {
 export const getMerchDetails = {
 	requestBody: object({
 		...sharedSalesMerchProps,
-		package_ids: array(number())
+		package_ids: array(number([integer()]))
 	}),
 	response: undefined
 };
 
 export const getShippingOrigin = {
 	requestBody: object({
-		band_id: optional(number()),
-		origin_id: optional(number())
+		band_id: optional(number([integer()])),
+		origin_id: optional(number([integer()]))
 	}),
 	response: undefined
 };
 
 	export const getOrders = {
 		requestBody: object({
-		band_id: number(),
-		member_band_id: optional(number()),
+		band_id: number([integer()]),
+		member_band_id: optional(number([integer()])),
 		start_time: optional(dateString),
 		end_time: optional(dateString),
 		unshipped_only: optional(boolean()),
 		name: optional(string()),
-		origin_id: optional(number()),
+		origin_id: optional(number([integer()])),
 		format: format
 	}),
 	response: undefined
@@ -83,14 +172,14 @@ export const getShippingOrigin = {
 export const updateShippedItems = {
 	requestBody: array(
 		object({
-			id: number(),
+			id: number([integer()]),
 			id_type: enumType(['p', 's']),
 			shipped: optional(boolean()),
 			notification: optional(boolean()),
 			notification_message: optional(string()),
 			ship_date: optional(dateString),
 			carrier: optional(string()),
-			tracking_code: optional(tuple([number(), string()]))
+			tracking_code: optional(tuple([number([integer()]), string()]))
 		})
 	),
 	response: undefined
@@ -98,11 +187,11 @@ export const updateShippedItems = {
 
 export const markDateRangeAsShipped = {
 	requestBody: object({
-		band_id: number(),
-		member_band_id: optional(number()),
+		band_id: number([integer()]),
+		member_band_id: optional(number([integer()])),
 		start_time: optional(dateString),
 		end_time: dateString,
-		origin_id: optional(number()),
+		origin_id: optional(number([integer()])),
 		email_notifications: optional(boolean())
 	}),
 	response: undefined
@@ -111,11 +200,11 @@ export const markDateRangeAsShipped = {
 export const updateQuantities = {
 	requestBody: array(
 		object({
-			id: number(),
+			id: number([integer()]),
 			id_type: idType,
-			quantity_sold: number(),
-			quantity_available: number(),
-			origin_id: optional(number())
+			quantity_sold: number([integer()]),
+			quantity_available: number([integer()]),
+			origin_id: optional(number([integer()]))
 		})
 	),
 	response: undefined
@@ -124,7 +213,7 @@ export const updateQuantities = {
 export const updateSku = {
 	requestBody:  array(
 		object({
-			id: number(),
+			id: number([integer()]),
 			id_type: idType,
 			sku: string()
 		})
--- src/schema/zod.ts
+++ src/schema/zod.ts
@@ -1,18 +1,19 @@
 import { z } from 'zod';
 
+const currency = z.number().min(0);
 const format = z.union([z.literal('csv'), z.literal('json')]).optional();
 const dateString = z.tuple([z.date(), z.string()]);
 const idType = z.union([z.literal('o'), z.literal('p')])
 
 const sharedCredentialProps = {
 	grant_type: z.literal('client_credentials'),
-	client_id: z.number(),
+	client_id: z.number().int(),
 	client_secret: z.string()
 };
 
 const sharedSalesMerchProps = {
-	band_id: z.number(),
-	member_band_id: z.number().optional(),
+	band_id: z.number().int(),
+	member_band_id: z.number().int().optional(),
 	start_time: dateString,
 	end_time: dateString.optional()
 };
@@ -35,6 +36,26 @@ export const refreshToken = {
 };
 
 /**
+ * Account API
+ * @see {@link https://bandcamp.com/developer/account}
+ */
+export const getMyBands = {
+	requestBody: undefined,
+	reponse: z.object({
+		bands: z.array(z.object({
+			subdomain: z.string(),
+			band_id: z.number().int(),
+			name: z.string(),
+			member_bands: z.object({
+				subdomain: z.string(),
+				band_id: z.number().int(),
+				name: z.string()
+			}).optional()
+		})),
+	})
+};
+
+/**
  * Sales API
  * @see {@link https://bandcamp.com/developer/sales}
  */
@@ -43,7 +64,82 @@ export const salesReport = {
 		...sharedSalesMerchProps,
 		format: format
 	}),
-	response: undefined
+	response: z.tuple([
+		//JSON
+		z.record(z.string().min(1), z.object({
+			date: z.preprocess(date => new Date(String(date)), z.date()),
+			paid_to: z.string(),
+			item_type: z.tuple([z.literal('album'), z.literal('track')]),
+			item_name: z.string(),
+			artist: z.string(),
+			currency: z.union([
+				z.literal('AUD'),
+				z.literal('CAD'),
+				z.literal('CHF'),
+				z.literal('CZK'),
+				z.literal('DKK'),
+				z.literal('EUR'),
+				z.literal('GBP'),
+				z.literal('HKD'),
+				z.literal('HUF'),
+				z.literal('ILS'),
+				z.literal('JPY'),
+				z.literal('MXN'),
+				z.literal('NOK'),
+				z.literal('NZD'),
+				z.literal('PLN'),
+				z.literal('SEK'),
+				z.literal('SGD'),
+				z.literal('USD')
+			]),
+			item_price: currency,
+			quantity: z.number().int().gte(0),
+			discount_code: z.tuple([z.null(), z.string()]),
+			sub_total: currency,
+			// seller_tax: null,
+			// marketplace_tax: null,
+			// shipping: null,
+			// ship_from_country_name: null,
+			transaction_fee: currency,
+			// fee_type: paypal,
+			item_total: currency,
+			amount_you_received: currency,
+			bandcamp_transaction_id: z.number().int(),
+			// paypal_transaction_id: null,
+			net_amount: currency,
+			// package: digital download,
+			// option: null,
+			item_url: z.string().url(),
+			// catalog_number: null,
+			upc: z.string(),
+			isrc: z.string(),
+			buyer_name: z.string(),
+			buyer_email: z.string().email(),
+			// buyer_phone: null,
+			// buyer_note: null,
+			ship_to_name: z.string(),
+			ship_to_street: z.string(),
+			ship_to_street_2: z.string(),
+			// ship_to_city: null,
+			// ship_to_state: null,
+			// ship_to_zip: null,
+			// ship_to_country: null,
+			// ship_to_country_code: null,
+			// ship_date: null,
+			// ship_notes: null,
+			// country: United States,
+			// country_code: US,
+			// region_or_state: IL,
+			city: z.string(),
+			referer: z.string(),
+			// referer_url: bandcamp.com/app/android/search,
+			// sku: null
+		})
+		),
+		// z.object({
+		// 	csv: z.string()
+		// })
+	])
 };
 
 /**
@@ -51,30 +147,30 @@ export const salesReport = {
  * @see {@link https://bandcamp.com/developer/merch}
  */
 export const getMerchDetails = {
-		requestBody: z.object({
-			...sharedSalesMerchProps,
-			package_ids: z.number().array()
-		}),
-		response: undefined
+	requestBody: z.object({
+		...sharedSalesMerchProps,
+		package_ids: z.number().int().array()
+	}),
+	response: undefined
 };
 
 export const getShippingOrigin = {
 	requestBody: z.object({
-		band_id: z.number().optional(),
-		origin_id: z.number().optional()
+		band_id: z.number().int().optional(),
+		origin_id: z.number().int().optional()
 	}),
 	response: undefined
 };
 
 export const getOrders = {
 	requestBody: z.object({
-		band_id: z.number(),
-		member_band_id: z.number().optional(),
+		band_id: z.number().int(),
+		member_band_id: z.number().int().optional(),
 		start_time: dateString.optional(),
 		end_time: dateString.optional(),
 		unshipped_only: z.boolean().optional(),
 		name: z.string().optional(),
-		origin_id: z.number().optional(),
+		origin_id: z.number().int().optional(),
 		format: format
 	}),
 	response: undefined
@@ -83,14 +179,14 @@ export const getOrders = {
 export const updateShippedItems = {
 	requestBody: z.array(
 		z.object({
-			id: z.number(),
+			id: z.number().int(),
 			id_type: z.union([z.literal('p'), z.literal('s')]),
 			shipped: z.boolean().optional(),
 			notification: z.boolean().optional(),
 			notification_message: z.string().optional(),
 			ship_date: dateString.optional(),
 			carrier: z.string().optional(),
-			tracking_code: z.tuple([z.number(), z.string()]).optional()
+			tracking_code: z.tuple([z.number().int(), z.string()]).optional()
 		}),
 	),
 	response: undefined
@@ -98,24 +194,24 @@ export const updateShippedItems = {
 
 export const markDateRangeAsShipped = {
 	requestBody: z.object({
-	band_id: z.number(),
-	member_band_id: z.number().optional(),
-	start_time: dateString.optional(),
-	end_time: dateString,
-	origin_id: z.number().optional(),
-	email_notifications: z.boolean().optional()
-}),
+		band_id: z.number().int(),
+		member_band_id: z.number().int().optional(),
+		start_time: dateString.optional(),
+		end_time: dateString,
+		origin_id: z.number().int().optional(),
+		email_notifications: z.boolean().optional()
+	}),
 	response: undefined
 };
 
 export const updateQuantities = {
 	requestBody: z.array(
 		z.object({
-			id: z.number(),
+			id: z.number().int(),
 			id_type: idType,
-			quantity_sold: z.number(),
-			quantity_available: z.number(),
-			origin_id: z.number().optional()
+			quantity_sold: z.number().int(),
+			quantity_available: z.number().int(),
+			origin_id: z.number().int().optional()
 		})
 	),
 	response: undefined
@@ -124,7 +220,7 @@ export const updateQuantities = {
 export const updateSku = {
 	requestBody: z.array(
 		z.object({
-			id: z.number(),
+			id: z.number().int(),
 			id_type: idType,
 			sku: z.string()
 		})
