c254f78e7cae49175225e35d291b80c12af08055
Jan T. Sott <git@idleberg.com>
2024-05-26 17:39:45 +0200

    update eslint plugins, specify uvu deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,25 @@
+/* eslint-disable */
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:json/recommended'
-  ],
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'unicorn'],
+	extends: ['eslint:recommended', 'plugin:jsonc/recommended-with-json', 'plugin:@typescript-eslint/recommended'],
+	globals: {
+		Deno: 'readonly',
+	},
 	rules: {
-		'@typescript-eslint/ban-types': [
-			'error',
-			{
-				extendDefaults: true,
-				types: {
-					'{}': false
-				}
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'unicorn/filename-case': 'error',
+		'unicorn/new-for-builtins': 'error',
+		'unicorn/prefer-node-protocol': 'error',
+	},
+	overrides: [
+		{
+			files: ['tsconfig.json'],
+			rules: {
+				'jsonc/no-comments': 'off'
 			}
-		]
-	}
+		},
+	],
 };
